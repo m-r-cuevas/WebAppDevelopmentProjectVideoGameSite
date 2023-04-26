52a316641a4e2bb7365de367b50b4070a22257ec
Maria Cuevas <mcuevas@seattleu.edu>
2023-04-26 04:40:34 -0700

    Code for basic index page based on code provided on class github added to Index.cshtml and Index.cshtml.cs. Page is not yet accesable from main page of website.  -Maria

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,19 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using System.Collections.Generic;
 
-namespace ContosoCrafts.WebSite.Pages
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Product
 {
-    public class PrivacyModel : PageModel
+    /// <summary>
+    /// Index Page will return all the data to show the user
+    /// </summary>
+    public class IndexModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public IndexModel(JsonFileProductService productService)
         {
-            _logger = logger;
+            ProductService = productService;
         }
 
-        public void OnGet()
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet
+        /// Return all the data
+        /// </summary>
+        /*public void OnGet()
         {
+            Products = ProductService.GetAllData();
         }
+        */
     }
 }
