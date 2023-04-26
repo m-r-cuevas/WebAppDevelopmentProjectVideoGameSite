52e6cba82f4d1b6196e7f75f2d23919946b9efbf
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-04-26 13:42:55 -0700

    Updated the View.cshtml and View.cshtml.cs to implement the read functionality.

---
+++ src/Pages/Product/View.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+using ContosoCrafts.WebSite.Models;
+
+using ContosoCrafts.WebSite.Services;
+
+ 
+
+namespace ContosoCrafts.WebSite.Pages.Product
+
+{
+
+    public class ViewModel : PageModel
+
+    {
+
+        // Getting the data from the JSON file.
+
+        public JsonFileProductService ProductService { get; }
+
+
+
+        // Initializing the product service.
+        public ViewModel(JsonFileProductService productService)
+
+        {
+            ProductService = productService;
+        }
+
+
+        public ProductModel Product;
+
+
+        public void OnGet(string id)
+        { 
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+
+        }
+ 
+
+        public object GetAverageRating(string productId)
+        {
+
+
+            var products = ProductService.GetProducts();
+
+            var ratings = products.First(x => x.Id == productId).Ratings;
+
+            double average = 0;
+
+            if (ratings != null)
+
+            {
+
+                average = ratings.Average();
+
+            }
+
+            return average;
+
+        }
+
+    }
+
+}
+
+
+
+
+
