1b802a8cdde289f37890bd82d5bb6ea0e96c2fcc
Sri Sai Srikanth Reddy Vundyala <svundyala@seattleu.edu>
2023-04-26 22:33:36 -0700

    Added documentation in Download.cshtml.cs

--- src/Pages/Download.cshtml.cs
+++ src/Pages/Download.cshtml.cs
@@ -3,17 +3,31 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// The DownloadModel is the page model for the download page of the Contoso Crafts website.
+    /// </summary>
     public class DownloadModel : PageModel
     {
+        /// <summary>
+        /// The logger is used to log messages from the DownloadModel class.
+        /// </summary>
         private readonly ILogger<DownloadModel> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the DownloadModel class.
+        /// </summary>
+        /// <param name="logger">The logger instance to use.</param>
         public DownloadModel(ILogger<DownloadModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Called when the page is requested.
+        /// </summary>
         public void OnGet()
         {
+            // This method is intentionally left blank.
         }
     }
 }
