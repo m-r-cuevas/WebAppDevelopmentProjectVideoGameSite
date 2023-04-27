95a1952cc194cd3a69ffe82736af070c49762c27
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-04-26 21:43:19 -0700

    Added documentation to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -3,20 +3,28 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Product Class
+    /// Product Model, Specifies the attributes of a product.
+    /// </summary>
     public class ProductModel
     {
-        public string Id { get; set; }
+        public string Id { get; set; }           //String Id of the product.
         // Added a property called "Name" -Ravi.
-        public string Name { get; set; }
-        public string Maker { get; set; }
+        public string Name { get; set; }         //String Name of the product.
+        public string Maker { get; set; }        //String Maker of the product.
         
         [JsonPropertyName("img")]
-        public string Image { get; set; }
-        public string Url { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int[] Ratings { get; set; }
+        public string Image { get; set; }        //Image of the product.
+        public string Url { get; set; }          //String Url of the image of the product.
+        public string Title { get; set; }        //String Title of the product.
+        public string Description { get; set; }  //String Description of the product.
+        public int[] Ratings { get; set; }       //Integer array of ratings of the product.
 
+        /// <summary>
+        /// Returns string representation of product object.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
  
