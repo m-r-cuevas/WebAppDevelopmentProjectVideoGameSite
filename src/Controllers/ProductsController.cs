31372f7a744194af2b536cc90529e7ada68f3290
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-04-26 21:42:31 -0700

    Added documentation to ProductsController.cs

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -7,23 +7,44 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// ProductsController
+    /// Deals with CRUDi requests made to website.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Constructor to the ProductsController class
+        /// </summary>
+        /// <param name="productService"></param>
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        /// <summary>
+        /// Getter method for the ProductService.
+        /// </summary>
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// This is the Read method in CRUDi.
+        /// Uses HttpGet to get all the products.
+        /// </summary>
+        /// <returns>List of all products.</returns>
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetProducts();
         }
 
+        /// <summary>
+        /// This method receives a request body with two properties "ProductId" and "Rating".
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>HTTP Response Code.</returns>
         [HttpPatch]
         public ActionResult Patch([FromBody] RatingRequest request)
         {
@@ -32,6 +53,9 @@ namespace ContosoCrafts.WebSite.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// This class is used to encapsulate "ProductId" and "Rating" values submitted by the users.
+        /// </summary>
         public class RatingRequest
         {
             public string ProductId { get; set; }
