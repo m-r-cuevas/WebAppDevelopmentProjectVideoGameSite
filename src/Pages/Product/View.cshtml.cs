3df6fcd70093fd350b62aa56418923c8a10ddd71
Sri Sai Srikanth Reddy Vundyala <svundyala@seattleu.edu>
2023-04-26 22:39:11 -0700

    Added documentation in View.cshtml.cs

--- src/Pages/Product/View.cshtml.cs
+++ src/Pages/Product/View.cshtml.cs
@@ -1,73 +1,54 @@
 ﻿using System.Linq;
-
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-
 using ContosoCrafts.WebSite.Models;
-
 using ContosoCrafts.WebSite.Services;
 
- 
-
 namespace ContosoCrafts.WebSite.Pages.Product
-
 {
-
-    public class ViewModel : PageModel
-
+    public class ViewModel : PageModel
     {
-
         // Getting the data from the JSON file.
-
         public JsonFileProductService ProductService { get; }
-
-
 
         // Initializing the product service.
         public ViewModel(JsonFileProductService productService)
-
         {
             ProductService = productService;
         }
 
-
+        // The product that will be displayed on the page.
         public ProductModel Product;
 
-
+        /// <summary>
+        /// Called when the page is requested.
+        /// </summary>
+        /// <param name="id">The ID of the product to display.</param>
         public void OnGet(string id)
-        { 
+        {
+            // Find the product with the specified ID.
             Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
-
         }
- 
 
+        /// <summary>
+        /// Calculates the average rating for the specified product.
+        /// </summary>
+        /// <param name="productId">The ID of the product to calculate the average rating for.</param>
+        /// <returns>The average rating for the product.</returns>
         public object GetAverageRating(string productId)
         {
-
-
             var products = ProductService.GetProducts();
 
+            // Get the ratings for the specified product.
             var ratings = products.First(x => x.Id == productId).Ratings;
 
+            // Calculate the average rating.
             double average = 0;
-
             if (ratings != null)
-
             {
-
                 average = ratings.Average();
-
             }
 
             return average;
-
         }
-
     }
-
 }
-
-
-
-
-
