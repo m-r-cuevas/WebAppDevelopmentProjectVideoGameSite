2d788e8b5612535058d236104509ee2da56c9567
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-04-26 21:43:44 -0700

    Added documentation to Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -7,20 +7,36 @@ using Microsoft.Extensions.Logging;
 // Adding comment for Git Lab : Maria
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Error Page class
+    /// Defines the errors for bad website requests
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
         public string RequestId { get; set; }
 
+        /// <summary>
+        /// Takes RequestId as an input
+        /// And returns true if it is a valid request
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// Constructor to the ErrorModel
+        /// Initializes the logger
+        /// </summary>
+        /// <param name="logger"></param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Gets the Id of the http request
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
