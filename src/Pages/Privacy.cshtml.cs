53a644bc198f177308c55be0e2b86d784981614a
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-04-26 21:41:28 -0700

    Added documentation to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,27 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// PrivacyModel class
+    /// For privacy page
+    /// </summary>
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        private readonly ILogger<PrivacyModel> _logger; //Readonly ILogger object that allows logging.
 
+        /// <summary>
+        /// PrivacyModel constructor
+        /// initializes logger
+        /// </summary>
+        /// <param name="logger"></param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        // <summary>
+        /// Placeholder for OnGet method
+        /// </summary>
         public void OnGet()
         {
         }
