757a182810088e1b3274325c41265b52befae29d
Sri Sai Srikanth Reddy Vundyala <svundyala@seattleu.edu>
2023-04-26 22:42:43 -0700

    Added documentation in Index.cshtml.cs

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,31 +1,38 @@
 ﻿using System.Collections.Generic;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
-    
     public class IndexModel : PageModel
     {
-        /// <param name="productService"></param>
+        /// <summary>
+        /// Initializes a new instance of the IndexModel class.
+        /// </summary>
+        /// <param name="productService">The product service.</param>
         public IndexModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        /// <summary>
+        /// Gets the product service.
+        /// </summary>
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// Gets or sets the products.
+        /// </summary>
         public IEnumerable<ProductModel> Products { get; private set; }
 
-        /// Fixed name of method GetAllProducts() --> GetProducts. -Maria
+        /// <summary>
+        /// Handles the GET request for the index page.
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetProducts();
         }
-        
     }
 }
