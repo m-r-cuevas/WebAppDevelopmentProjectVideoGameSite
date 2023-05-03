48f56d1cb44301bb5b2b3ad8e64297f48a92670b
Maria Cuevas <mcuevas@seattleu.edu>
2023-05-03 16:59:58 -0700

    Add unit tests for read (model = View.cshtml)

---
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -0,0 +1,52 @@
+﻿
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+
+namespace UnitTests.Pages.Product.Read
+{
+    public class ReadTests
+    {
+        #region TestSetup
+        public static ViewModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ViewModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("1");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Fifa", pageModel.Product.Name);
+        }
+
+        [Test]
+        public void GetRatings_No_Ratings_Valid_Should_Return_True()
+        {
+            // Arrange
+
+            // Act
+            var ratings = pageModel.GetAverageRating("1");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(0, ratings);
+        }
+        #endregion OnGet
+    }
+}
