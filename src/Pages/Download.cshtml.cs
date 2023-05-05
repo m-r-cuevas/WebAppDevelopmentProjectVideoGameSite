7f37a3afefea8db7a3cb4233454ea34f6c9246bf
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-05-04 22:24:14 -0700

    Changed the name of the namespace to ConsoleCafe in Download.cshtml.cs

--- src/Pages/Download.cshtml.cs
+++ src/Pages/Download.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace ContosoCrafts.WebSite.Pages
+namespace ConsoleCafe.WebSite.Pages
 {
     /// <summary>
     /// The DownloadModel is the page model for the download page of the Contoso Crafts website.
