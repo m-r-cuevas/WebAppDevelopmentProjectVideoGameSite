00ff8e3181a34b4ab13de737bfee05a456870c5a
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-05-04 22:26:33 -0700

    Changed the name of the namespace to ConsoleCafe in Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace ContosoCrafts.WebSite.Pages
+namespace ConsoleCafe.WebSite.Pages
 {
     /// <summary>
     /// PrivacyModel class
