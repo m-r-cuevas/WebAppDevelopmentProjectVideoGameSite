7bef7752fbdb545661abfb66b26ad36d6a9c7988
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-05-04 22:34:14 -0700

    Changed the name of the namespace to ConsoleCafe in Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace ContosoCrafts.WebSite
+namespace ConsoleCafe.WebSite
 {
     /// <summary>
     /// Program class
