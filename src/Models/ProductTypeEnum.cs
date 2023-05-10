efd53429d4120d1aacfe3258a8f0ad2e4ca4c879
Maria Cuevas <mcuevas@seattleu.edu>
2023-05-10 04:37:25 -0700

    Created a product type enum in ProductTypeEnum.cs.

---
+++ src/Models/ProductTypeEnum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleCafe.WebSite.Models
+{
+    public enum ProductTypeEnum
+    {
+        Undefined = 0,
+        FPS = 1,
+        Racing = 2,
+        Sports = 3,
+    }
+
+    public static class ProductTypeEnumExtensions
+    {
+        public static string DisplayName(this ProductTypeEnum data)
+        {
+            return data switch
+            {
+                ProductTypeEnum.FPS => "First Person Shooter Game",
+                ProductTypeEnum.Racing => "Racing Game",
+                ProductTypeEnum.Sports => "Sports Game",
+
+                // Default, Unknown
+                _ => "",
+            } ;
+        }
+    }
+}
