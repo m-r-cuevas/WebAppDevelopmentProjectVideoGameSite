f66f75c88a715758a4e1be7e6fa695e32465c507
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-05-10 21:21:05 -0700

    Added validation for the fields being displayed in create form in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -5,33 +6,52 @@ namespace ConsoleCafe.WebSite.Models
 {
     /// <summary>
     /// Product Class
-    /// Product Model, Specifies the attributes of a product.
+    /// Product Model, Specifies the attributes of a game.
     /// </summary>
     public class ProductModel
     {
-        //String Id of the product.
-        public string Id { get; set; }           
-        // Added a property called "Name".
+        //Required attribute is to specify that a property is required.
+
+        //Id of the game is required.
+        [Required(ErrorMessage = "Id field is required.")]
+        public string Id { get; set; }
+
+        //Name of the game is required.
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "100 character limit.")]
         public string Name { get; set; }
-        //String Maker of the product.
+
+        //Maker of the game is required.
+        [Required(ErrorMessage = "Maker is required")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "100 character limit.")]
         public string Maker { get; set; }        
         
+        //Image of the game is required.
         [JsonPropertyName("img")]
-        //Image of the product.
+        [Url]
         public string Image { get; set; }
-        //String Url of the image of the product.
+
+        //String Url of the game.
+        [Url]
         public string Url { get; set; }
-        //String Title of the product.
+
+        //String Title of the game - Not required as it not being displayed but has a maximum length of 100.
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "100 character limit.")]
         public string Title { get; set; }
-        //String Description of the product.
+
+        //String Description of the game is required.
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "200 charcter limit.")]
         public string Description { get; set; }
-        // Enum product type
-        public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;  
-        //Integer array of ratings of the product.
+
+        //Enum product type.
+        public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
+
+        //Integer array of ratings of the product - Not required.
         public int[] Ratings { get; set; }       
 
         /// <summary>
-        /// Returns string representation of product object.
+        /// Returns string representation of the game.
         /// </summary>
         /// <returns></returns>
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
