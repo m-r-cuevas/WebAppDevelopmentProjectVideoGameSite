05c863f8bd2752bf42deceb91b80d60422195f10
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-05-17 00:19:08 -0700

    Updated the testcases to get 100% code coverage in Read.cshtml.Tests.cs

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -1,9 +1,7 @@
-﻿
-
+﻿using System.Linq;
 using NUnit.Framework;
-
 using ConsoleCafe.WebSite.Pages.Product;
-
+using System;
 namespace UnitTests.Pages.Product.Read
 {
     public class ReadTests
@@ -18,35 +16,98 @@ namespace UnitTests.Pages.Product.Read
             {
             };
         }
-
         #endregion TestSetup
 
         #region OnGet
         [Test]
-        public void OnGet_Valid_Should_Return_Products()
+        public void OnGet_Should_Select_Matching_Id()
         {
             // Arrange
+            var data = TestHelper.ProductService.GetProducts().First();
 
             // Act
-            pageModel.OnGet("1");
+            pageModel.OnGet(data.Id);
 
             // Assert
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
-            Assert.AreEqual("Fifa", pageModel.Product.Name);
+            Assert.AreEqual(data.Id, pageModel.Product.Id);
+        }
+        #endregion OnGet
+
+        #region GetAverageRating
+        [Test]
+        public void GetAverageRating_Ratings_Is_Null_Should_Return_Zero()
+        {
+            // Arrange
+            var product = TestHelper.ProductService.GetProducts().First();
+
+            // Act
+            var ratings = pageModel.GetAverageRating(product.Id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(0, ratings);
         }
 
         [Test]
-        public void GetRatings_No_Ratings_Valid_Should_Return_True()
+        public void GetAverageRating_Ratings_Not_Null_Should_Return_The_Avg_Rating_Of_Product_Matching_Product_Id()
         {
             // Arrange
+            var product = TestHelper.ProductService.GetProducts().First();
+
+            int numRatings = 5;
+
+            for (int i = 0; i < numRatings; i++)
+            {
+                TestHelper.ProductService.AddRating(product.Id, i + 1);
+            }
+
+            var average = TestHelper.ProductService.GetProducts().First().Ratings.Average();
 
             // Act
-            var ratings = pageModel.GetAverageRating("1");
+            var result = pageModel.GetAverageRating(product.Id);
 
             // Assert
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
-            Assert.AreEqual(5, ratings);
+            Assert.AreEqual(average, result);
         }
-        #endregion OnGet
+        #endregion GetAverageRating
+
+        #region OnPost
+
+        /// <summary>
+        /// Test the OnPost method to return the correct comment
+        /// </summary>
+        [Test]
+        public void OnPost_Should_Return_RedirectToPageResult()
+        {
+            // Arrange
+            var id = "4";
+
+
+            // Act
+            var location = TestHelper.ProductService.GetProducts().First(m => m.Id.Equals(id));
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+
+        }
+
+        /// <summary>
+        /// Test for the OnPost method to return the correct comment if the name is null or empty
+        /// </summary>
+        [Test]
+        public void OnPost_Should_Set_Name_To_Anonymous_If_Name_Is_Null_Or_Empty()
+        {
+            // Arrange
+            var id = "4";
+
+            // Act
+            var location = TestHelper.ProductService.GetProducts().First(m => m.Id.Equals(id));
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnPost
     }
 }
