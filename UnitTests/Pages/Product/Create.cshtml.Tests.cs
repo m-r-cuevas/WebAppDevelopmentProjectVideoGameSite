6493298dd5d4eae9dd7bd57e4a08c4296a2e8d5a
rnimmalapudi <rnimmalapudi@seattleu.edu>
2023-05-17 00:16:43 -0700

    Updated the testcases to get 100% code coverage in Create.cshtml.Tests.cs

--- UnitTests/Pages/Product/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Create.cshtml.Tests.cs
@@ -1,43 +1,56 @@
+using Microsoft.Extensions.Logging;
+using Moq;
 using NUnit.Framework;
 using ConsoleCafe.WebSite.Pages.Product;
+using ConsoleCafe.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests.Pages.Product.Create
 {
     public class CreateTests
     {
         #region TestSetup
-        public static CreateModel pageModel;
+        public static CreateModel createModel;
+        private JsonFileProductService _productService;
+        private Mock<ILogger<CreateModel>> _mockLogger;
 
         [SetUp]
         public void TestInitialize()
         {
-            pageModel = new CreateModel(TestHelper.ProductService)
-            {
-            };
+            _mockLogger = new Mock<ILogger<CreateModel>>();
+            _productService = TestHelper.ProductService;
+            createModel = new CreateModel(_mockLogger.Object, _productService);
         }
 
         #endregion TestSetup
         #region OnGet
         [Test]
-		public void OnGet_Returns_Product_With_Id()
+		public void OnGet_ReturnsRedirectToPageResult()
 		{
-			// Arrange
+            string expId = "temp";
 			
 
-
-
 			// Act
-            // Since the id is a string, for every new card that is created 1 is concatenated
-            // to the id. which is why the new product should be 91 since the last productId is 9
-			pageModel.OnGet();
+			var result = createModel.OnGet() as RedirectToPageResult;
 
-			// Assert
-			Assert.AreEqual(true, pageModel.ModelState.IsValid);
-			Assert.AreEqual(pageModel.Product.Id, "10");
-
+            // Assert
+            Assert.AreEqual("./Update", result.PageName);
+            Assert.AreEqual(expId, result.RouteValues["Id"]);
+            Assert.AreEqual(true, createModel.ModelState.IsValid);
 		}
+
+        ///<summary>
+        ///Testing the Getter.
+        ///</summary>
+        [Test]
+        public void CreateModel_Constructor_SetsProductServiceProperty()
+        {
+            // Arrange & Act
+            var createModel = new CreateModel(_mockLogger.Object, _productService);
+
+            //Assert
+            Assert.AreEqual(_productService, createModel.ProductService);
+        }
 		#endregion OnGet
-
-
 	}
 }
