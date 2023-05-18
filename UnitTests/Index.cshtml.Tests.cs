1b1ab2bb9faa4c5ca93c34ae2c24921fbe865bd5
Maria Cuevas <mcuevas@seattleu.edu>
2023-05-17 22:27:45 -0700

    Added unit tests for Index page to get to 100% currently at 88% (changes made to Index.cshtml.Tests.cs)

---
+++ UnitTests/Index.cshtml.Tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using ConsoleCafe.WebSite.Pages;
+
+namespace UnitTests.Pages.Index
+{
+    public class IndexTests
+    {
+        #region TestSetup
+        public static IndexModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            var MockProductService = TestHelper.ProductService;
+
+            pageModel = new IndexModel(MockLoggerDirect, MockProductService)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
