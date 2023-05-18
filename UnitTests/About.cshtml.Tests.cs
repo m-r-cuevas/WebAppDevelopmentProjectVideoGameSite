65e844cc87e5d36882edf657b85995130b4fddbe
Maria Cuevas <mcuevas@seattleu.edu>
2023-05-17 22:25:18 -0700

    Added unit tests for About page to get to 100% (changes made to About.cshtml.Tests.cs)

---
+++ UnitTests/About.cshtml.Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using ConsoleCafe.WebSite.Pages;
+
+namespace UnitTests.Pages.About
+{
+    public class AboutTests
+    {
+        #region TestSetup
+        public static AboutModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<AboutModel>>();
+
+            pageModel = new AboutModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
