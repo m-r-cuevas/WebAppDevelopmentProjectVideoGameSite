68cb5b51051a4ce06a27749ea747f791025298fe
Maria Cuevas <mcuevas@seattleu.edu>
2023-05-17 22:26:35 -0700

    Added unit tests for Download page to get to 100% (changes made to Download.cshtml.Tests.cs)

---
+++ UnitTests/Download.cshtml.Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using ConsoleCafe.WebSite.Pages;
+
+namespace UnitTests.Pages.Download
+{
+    public class DownloadTests
+    {
+        #region TestSetup
+        public static DownloadModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<DownloadModel>>();
+
+            pageModel = new DownloadModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
