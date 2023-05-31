87478f997e3b87b644d0e07881776c6a1346cc9e
Maria Cuevas <mcuevas@seattleu.edu>
2023-05-31 01:53:17 -0700

    Added content to Download page to display game information (Changes made to Download.cshtml, Download.cshtml.cs)

--- src/Pages/Download.cshtml.cs
+++ src/Pages/Download.cshtml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ConsoleCafe.WebSite.Models;
+using ConsoleCafe.WebSite.Services;
 
 namespace ConsoleCafe.WebSite.Pages
 {
@@ -9,25 +11,54 @@ namespace ConsoleCafe.WebSite.Pages
     public class DownloadModel : PageModel
     {
         /// <summary>
-        /// The logger is used to log messages from the DownloadModel class.
+        /// Getting the data from the JSON file.
         /// </summary>
-        private readonly ILogger<DownloadModel> _logger;
+        public JsonFileProductService ProductService { get; } 
 
         /// <summary>
-        /// Initializes a new instance of the DownloadModel class.
+        /// Initializing the product service.
         /// </summary>
-        /// <param name="logger">The logger instance to use.</param>
-        public DownloadModel(ILogger<DownloadModel> logger)
+        /// <param name="productService"></param>
+        public DownloadModel(JsonFileProductService productService)
         {
-            _logger = logger;
+            ProductService = productService;
         }
+
+        /// <summary>
+        /// The product that will be displayed on the page.
+        /// </summary>
+        public ProductModel Product;
 
         /// <summary>
         /// Called when the page is requested.
         /// </summary>
-        public void OnGet()
+        /// <param name="id">The ID of the product to display.</param>
+        public void OnGet(string id)
         {
-            // This method is intentionally left blank.
+            // Find the product with the specified ID.
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Calculates the average rating for the specified product.
+        /// </summary>
+        /// <param name="productId">The ID of the product to calculate the average rating for.</param>
+        /// <returns>The average rating for the product.</returns>
+        public object GetAverageRating(string productId)
+        {
+            var products = ProductService.GetProducts();
+
+            // Get the ratings for the specified product.
+            var ratings = products.First(x => x.Id == productId).Ratings;
+
+            // Calculate the average rating.
+            double average = 0;
+            if (ratings != null)
+            {
+                average = ratings.Average();
+            }
+
+            return average;
         }
     }
 }
